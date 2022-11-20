f327b02e4e35289f9b02f0e1c020ac936a2487b1
Di4bel <pruefertinte8@gmx.de>
2022-11-20 16:33:29 +0100

    swap

--- functions.php
+++ functions.php
@@ -1,2 +1,13 @@
 <?php
-echo ("Hello world");
+if ( ! function_exists( 'whispers_theme' ) ) :
+    /**
+    * Sets up theme defaults and registers support for various WordPress features
+    *
+    *  It is important to set up these functions before the init hook so that none of these
+    *  features are lost.
+    *
+    * 
+    */
+    function whispers_theme() {
+        echo("Hello World");
+    }
